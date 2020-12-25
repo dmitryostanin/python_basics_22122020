e453ffdcd48179f0607e5f09b21d834f06ee4bf7
Dmitry Ostanin <dmitry.ostanin@mail.ru>
2020-12-25 15:47:56 +0300

    task 1

---
+++ homeworks/lesson_1/1.py
@@ -0,0 +1,9 @@
+variable_int = 2
+variable_float = 444.44
+variable_bool = True
+variable_str = 'Hello!'
+print(f'int : {variable_int} \nfloat: {variable_float} \nbool: {variable_bool} \nstr: {variable_str}')
+
+name = input('Enter name: ')
+age = input('Enter age: ')
+print(f'{name} is {age} years old! ')
